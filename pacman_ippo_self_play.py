d9047c3cd4f6ae0086c9702814e6318adf64fa53
Celeste <celestedeschamphelaere@protonmail.com>
2025-12-05 13:33:04 +0100

    ppomaxxing

--- pacman_ippo_self_play.py
+++ pacman_ippo_self_play.py
@@ -21,7 +21,7 @@ ENT_COEF = 0.01 #entropy penalty
 VF_COEF = 0.5
 MAX_GRAD_NORM = 0.5
 UPDATE_EPOCHS = 10     # How many times to re-use data
-TOTAL_UPDATES = 250   # Total training loops
+TOTAL_UPDATES = 5   # Total training loops
 
 HIDDEN_DIM = 512 #512 seems to work ok
 
